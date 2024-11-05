c94c389c826f934a46ab6014f4dbb5c859eacc4b
AbdElNaby <kamohamedabdelnabyly@gmail.com>
2024-11-05 18:21:54 +0200

    wip

--- android/app/src/main/java/com/wix_structure/MainApplication.kt
+++ android/app/src/main/java/com/wix_structure/MainApplication.kt
@@ -29,7 +29,6 @@ class MainApplication : NavigationApplication() {
 
   override fun onCreate() {
     super.onCreate()
-
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
